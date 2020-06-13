8c04475ce73feaf8dbbb6ea8da0b10044c0e749b
Sahil Kalkal <s.k.kalkal456@gmail.com>
2020-06-13 08:08:08 +0530

    Updates

---
+++ precache-manifest.65a3e29255f74f08925b9b6e12cf667d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d330f6c26f34f6e2b098be2c06b5544",
+    "url": "/chem/index.html"
+  },
+  {
+    "revision": "c56f120acc66078e9493",
+    "url": "/chem/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bdbe6b3d43d93a8c26a3",
+    "url": "/chem/static/css/main.6ce6d5c2.chunk.css"
+  },
+  {
+    "revision": "c56f120acc66078e9493",
+    "url": "/chem/static/js/2.f617898f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chem/static/js/2.f617898f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbe6b3d43d93a8c26a3",
+    "url": "/chem/static/js/main.d06f6a51.chunk.js"
+  },
+  {
+    "revision": "dc41d09dec4e6a54a1d8",
+    "url": "/chem/static/js/runtime-main.a32ade45.js"
+  }
+]);
